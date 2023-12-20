f2706274ba5ed400a87dceba291de01e63829681
Pablo Goitia <pgg566@alumnos.unican.es>
2023-12-20 10:54:16 +0100

    Now, the script will log the results into a report file.

--- gaussian_blur.sh
+++ gaussian_blur.sh
@@ -2,9 +2,10 @@
 
 src_dir="img/src"
 dest_dir="img/dest"
+report_file="report.txt"
 
 for file in $src_dir/*; do
     filename=$(basename "$file")
     dest_file="$dest_dir/$filename"
-    ./gauss "$file" "$dest_file"
+    ./gauss "$file" "$dest_file" >> "$report.txt"
 done
